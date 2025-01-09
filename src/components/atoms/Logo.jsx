cd706d09853dbc57570484b65a1a276557521893
swapnil <swapnil130501@gmail.com>
2025-01-09 18:01:47 +0530

    create Logo component

---
+++ src/components/atoms/Logo.jsx
@@ -0,0 +1,12 @@
+import LogoImage from '../../assets/logo.png';
+
+export const Logo = ({
+    width,
+    height
+}) => {
+    return (
+        <div className="w-32 px-5 mb-5">
+            <img src={LogoImage} alt="logo" className="{`w-${width} h-${height}`}"/>
+        </div>
+    )
+}
--- src/components/organisms/Sidebar.jsx
+++ src/components/organisms/Sidebar.jsx
@@ -1,14 +1,11 @@
-import Logo from '../../assets/logo.png';
+import { Logo } from "../atoms/Logo"
 
 export const Sidebar = () => {
     return (
         <div className="w-full bg-black px-2 text-gray-400 h-[calc(100vh-100px)]">
 
             <div className="h-full py-5">
-                
-                <div className="w-32 px-5 mb-5">
-                    <img src={Logo} alt="logo" className="w-16 h-16"/>
-                </div>
+                <Logo width={16} height={16}/>
             </div>
         </div>
     )
