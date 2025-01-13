1c597f286a13264e17de521a1e85624078b2a181
swapnil <swapnil130501@gmail.com>
2025-01-13 15:15:33 +0530

    add progress song logic

--- src/components/molecules/PlayerController.jsx
+++ src/components/molecules/PlayerController.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import ReactHowler from "react-howler";
 import Audio1 from '../../assets/1.mp3';
 import Audio2 from '../../assets/2.mp3';
+import { formatTime } from "../../utils/timeFormatter"
 
 import {
     MdShuffle,
@@ -93,12 +94,49 @@ export const PlayerController = () => {
         })
     }
 
+    function handleSeek(e) {
+        console.log(e.target.value);
+        const value = parseFloat(e.target.value);
+        setPlayerState({
+            ...playerState,
+            seek: value
+        })
+        soundRef.current.seek(value);
+    }
+
+    function handleSongLoad() {
+        setPlayerState({
+            ...playerState,
+            duration: soundRef.current.duration()
+        })
+    }
+
+    useEffect(() => {
+        let id;
+        if(playerState.playing && !playerState.isSeeking) {
+            const updateSeek = () => {
+                setPlayerState({
+                    ...playerState,
+                    seek: soundRef.current.seek()
+                });
+                id = requestAnimationFrame(updateSeek);
+            }
+
+            id = requestAnimationFrame(updateSeek);
+            return () => {
+                cancelAnimationFrame(id);
+            }
+        }
+        cancelAnimationFrame(id);
+    }, [playerState.isSeeking, playerState.playing]);
+
     return (
         <div className="text-white">
             <ReactHowler 
                 ref={soundRef}
                 src={songs[playerState.index].url}
                 playing={playerState.playing}
+                onLoad={handleSongLoad}
             />
             <div
                 className="flex justify-center items-center space-x-4 py-4 text-gray-400"
@@ -134,8 +172,6 @@ export const PlayerController = () => {
                         )
                 }
 
-                
-
                 <button
                     onClick={nextSong}
                     className="text-2xl"
@@ -149,6 +185,28 @@ export const PlayerController = () => {
                     <MdOutlineRepeat />
                 </button>
 
+            </div>
+            {/* progress bar ui*/}
+
+            <div className="flex items-center justify-center space-x-2 text-xs text-gray-400 px-4">
+                <div className="w-10 text-right">
+                    {formatTime(playerState.seek)}
+                </div>
+                <input 
+                    type="range"
+                    className="w-full bg-gray-800 accent-gray-600"
+                    min={"0"}
+                    value={playerState.seek}
+                    max={playerState.duration || 0}
+                    step="0.1"
+                    onChange={handleSeek}
+                    onMouseDown={() => { setPlayerState({ ...playerState, isSeeking: true }) }}
+                    onMouseUp={() => { setPlayerState({ ...playerState, isSeeking: false }) }}
+                />
+
+                <div className="w-10 text-left">
+                    {formatTime(playerState.duration)}
+                </div>
             </div>
         </div>
     )
